44bf7973433dcf8509a7cc7adebe99891233bfa1
Wilber Gutierrez <wilberjasonmarlon@gmail.com>
2022-11-28 07:48:41 -0500

    minsdk 26

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,7 +2,7 @@ object Android {
     const val appId = "cu.wilb3r.musalawatherapp"
     const val compileSdk = 32
     const val buildTools = "31.0.0"
-    const val minSdk = 23
+    const val minSdk = 26
     const val targetSdk = 32
     const val versionCode = 1
     const val versionName = "1.0"
