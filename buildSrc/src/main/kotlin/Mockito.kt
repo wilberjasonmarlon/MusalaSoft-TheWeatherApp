24e24873bac4fdbd61917fa04dfcbc5c1d5aa551
Wilber Gutierrez <wilberjasonmarlon@gmail.com>
2022-11-28 07:51:32 -0500

    minsdk 26

--- buildSrc/src/main/kotlin/Mockito.kt
+++ buildSrc/src/main/kotlin/Mockito.kt
@@ -1,5 +1,5 @@
 object Mockito {
-    private const val version = "3.2.0"
+    private const val version = "3.5.2"
     const val mockito = "org.mockito.kotlin:mockito-kotlin:$version"
     const val mockk = "io.mockk:mockk:1.12.2"
 }
